3e99d59b2378a20bd3c2fad786c4c78426ca5d04
HugoMProject <hugomoyano1996@gmail.com>
2022-07-07 21:19:50 -0300

    Se agrego nodemon y se crearon pagina de contactos y home

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
+
+const path= require('path')
+
 require('dotenv').config()
 
 const express = require('express');
 const app = express ();
 
+app.get('/', function(solicitud, respuesta){
+    respuesta.sendFile(path.join(__dirname ,'views/index.html'))
+})
+
+app.get('/contacto',  function(solicitud, respuesta){
+    respuesta.sendFile(path.join(__dirname ,'views/contacto.html')) 
+})
 
 app.listen(process.env.PUERTO, function(){
     console.log('Servidor inicilizado')
